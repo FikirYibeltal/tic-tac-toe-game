7899a03c111ce7e9a08adfe128cdef9b6aeaea07
Fikir Yibeltal <Fyibeltal@excellerentsolutions.com>
2021-07-13 13:27:21 +0300

    finished tic tac toe

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,122 @@
-
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
 function App() {
+  const [currentValue, setCurrentValue] = useState("O");
+  const [looser, setLooser] = useState("");
+  const [checkedList, setCheckedList]: any = useState({});
+  const [winningPattern, setWinningPattern]: any = useState([]);
+  const [messages, setMessages]: any = useState(["Game has started ... "]);
+  useEffect(() => {
+    rules();
+  }, [checkedList]);
+  useEffect(() => {
+    if (looser === "none") {
+      setMessages(["The Game Ends in a Draw", ...messages]);
+      return;
+    }
+    if (looser) {
+      setMessages([
+        looser === "O" ? "Player 2 wins the Game" : "Player 1 wins the Game",
+        ...messages,
+      ]);
+    }
+  }, [looser]);
+  function handleClick(currentIndex) {
+    if (!looser) {
+      currentIndex = currentIndex.toString();
+      if (!checkedList[currentIndex]) {
+        setCheckedList({ ...checkedList, [currentIndex]: currentValue });
+        if (currentValue === "O") {
+          setCurrentValue("X");
+          setMessages(["Player 1 selected " + currentIndex, ...messages]);
+        } else {
+          setCurrentValue("O");
+          setMessages(["Player 2 selected " + currentIndex, ...messages]);
+        }
+      }
+    }
+  }
+
+  function rules() {
+    const winningMoves = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
+
+    for (let eachWinningMove of winningMoves) {
+      if (
+        (checkedList[eachWinningMove[0]] === "O" &&
+          checkedList[eachWinningMove[1]] === "O" &&
+          checkedList[eachWinningMove[2]] === "O") ||
+        (checkedList[eachWinningMove[0]] === "X" &&
+          checkedList[eachWinningMove[1]] === "X" &&
+          checkedList[eachWinningMove[2]] === "X")
+      ) {
+        setLooser(currentValue);
+        setWinningPattern([...eachWinningMove]);
+        return;
+      }
+    }
+    if (Object.keys(checkedList).length === 9 && !looser) {
+      setLooser("none");
+    }
+  }
+  function handleReset() {
+    setMessages(["Game has started ... "]);
+    setCurrentValue("O");
+    setLooser("");
+    setCheckedList({});
+    setWinningPattern([]);
+  }
+  function cssForEachGrid(currentIndex) {
+    const winnerColor = "winner";
+    return winningPattern?.includes(currentIndex) ? winnerColor : null;
+  }
   return (
-
-    <Router>
-      <div>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+    <div className="home">
+      <div className={`tic ${looser === "none" ? "draw" : null}`}>
+        {[...Array(9)].map((x, index) => (
+          <EachGrid
+            key={index}
+            handleClick={handleClick}
+            cssForEachGrid={cssForEachGrid}
+            currentIndex={index + 1}
+            checkedList={checkedList}
+          />
+        ))}
       </div>
-    </Router>
-
+      <div className="output-console">
+        <button style={{ cursor: "pointer" }} onClick={handleReset}>
+          Reset
+        </button>
+        <div className="console">
+          {messages?.map((message, index) => (
+            <p key={index}>
+              <span>=&gt; </span>
+              {message}
+            </p>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
 
-function Home(){
-  return(<div> this is home page</div>)
-}
-function About(){
-  return(<div> this is about page</div>)
-}
-
-function Contact(){
-  return(<div>this is contact page</div>)
+function EachGrid({ handleClick, cssForEachGrid, checkedList, currentIndex }) {
+  return (
+    <div
+      onClick={() => handleClick(currentIndex)}
+      className={`each ${cssForEachGrid(currentIndex)}`}
+    >
+      {checkedList[currentIndex] ?? null}
+    </div>
+  );
 }
 
 export default App;
